684eac258b3ac244af52a36945e49872e7dc9878
Pratham <ppatne189@gmail.com>
2025-12-04 22:43:32 +0530

    test push

--- lib/repository/auth_repo.dart
+++ lib/repository/auth_repo.dart
@@ -5,18 +5,4 @@ class AuthRepository {
   static APIManager apiManager = APIManager();
 
 
-  // static Future<RegisterApiResponseModel> registerApiCall({required Map<String, dynamic> params, bool showLoading = false}) async {
-  //   try {
-  //     var response = await apiManager.postAPICall(
-  //       url: 'signup',
-  //       params: params,
-  //       showLoading: showLoading,
-  //     );
-  //     RegisterApiResponseModel registerApiResponseModel = RegisterApiResponseModel.fromJson(response);
-  //     return registerApiResponseModel;
-  //   } catch (e) {
-  //     debugPrint(e.toString());
-  //     rethrow;
-  //   }
-  // }
 }
